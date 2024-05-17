757c93c64056aa226cc0d9d68a2e8c9372e68787
ruedagarcialaura <ruedagarcialaura1502@gmail.com>
2024-05-17 11:47:59 +0200

    initial commit

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+
+
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+
+# Add widgets (buttons, labels, etc.) to the window
+# ...
+# Add buttons
+button1 = tk.Button(window, text="hola")
+button1.pack()
+
+button2 = tk.Button(window, text="bello flor")
+button2.pack()
+
+# Add a title
+window.title("GUI de laura")
+
+# Set the window properties (size, etc.)
+# ...
+
+# Start the main event loop
+window.mainloop()
